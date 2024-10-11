897bfdd6d5275f4a7e1d44a499872a94fef05e9d
Nessymanuel <graciethmanuel13@gmail.com>
2024-10-11 09:21:21 +0200

    actualização do lab helloworld

--- HelloWorld/app/src/main/java/ao/co/isptec/aplm/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/ao/co/isptec/aplm/helloworld/MainActivity.java
@@ -6,38 +6,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Chave usada para passar a mensagem para a próxima atividade
-    public static final String EXTRA_MESSAGE = "com.example.helloworldtwoactivities.MESSAGE";
-
+    public static final String EXTRA_MESSAGE = "ao.co.isptec.aplm.helloworld.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Referências para os componentes da UI
         EditText editTextMessage = findViewById(R.id.editTextMessage);
         Button buttonSend = findViewById(R.id.buttonSend);
 
-        // Definir o comportamento do botão "Enviar"
         buttonSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Captura o texto digitado
                 String message = editTextMessage.getText().toString();
-
-                // Cria uma Intent para iniciar a nova atividade
                 Intent intent = new Intent(MainActivity.this, DisplayMessageActivity.class);
-                intent.putExtra(EXTRA_MESSAGE, message); // Passa a mensagem para a nova atividade
-                startActivity(intent); // Inicia a nova atividade
+                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
             }
         });
     }
